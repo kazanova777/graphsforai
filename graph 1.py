1aca3a9521e0ad8fa65d370638a96d4fa5608da1
za04514 <zholdas.aldanbergen@stud.th-deg.de>
2024-01-14 23:43:04 +0300

    changes in code made

--- graph 1.py
+++ graph 1.py
@@ -1,18 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Let's sketch a graph that represents the complexity described in the paragraph.
-
 # Variables for the sketch
-actions = np.arange(0, 1000)  # Number of actions taken
-game_states = np.exp(actions * 0.005)  # Exponential growth of game states with actions
-computational_difficulty = game_states / 100  # Simplified illustration of computational difficulty
+actions = np.arange(0, 1000)  
+game_states = np.exp(actions * 0.005)  
+computational_difficulty = game_states / 100  
 
 # Non-linear progression representing the balance between exploration and exploitation
-# We'll simulate this as a sine wave fluctuating around an increasing trend
 balance_progression = 100 + 10 * np.sin(actions * 0.1) + actions * 0.1
 
-# Creating the plot
 fig, ax1 = plt.subplots(figsize=(12, 6))
 
 # Plotting the exponential growth of game states
@@ -25,23 +21,23 @@ ax1.grid(True)
 
 # Create a twin Axes sharing the x-axis
 ax2 = ax1.twinx()
-# Plotting the computational difficulty
+
 ax2.plot(actions, computational_difficulty, 'r--', label='Computational Difficulty')
 ax2.set_ylabel('Computational Difficulty', color='r')
 ax2.tick_params('y', colors='r')
 
-# Plotting the balance progression
 ax3 = ax1.twinx()
+
 # Offset the twin Axes to avoid overlapping y-axes
 ax3.spines['right'].set_position(('outward', 60))
 ax3.plot(actions, balance_progression, 'g-.', label='Exploration-Exploitation Balance')
 ax3.set_ylabel('Balance Progression', color='g')
 ax3.tick_params('y', colors='g')
 
-# Adding legends
+
 ax1.legend(loc='upper left')
 ax2.legend(loc='upper center')
 ax3.legend(loc='upper right')
 
-# Show plot
+
 plt.show()
